27bf768726f782ec8686cc195584736fb796046a
Zacks <53922473+Zackperez@users.noreply.github.com>
2023-05-03 12:22:04 -0500

    MVC Implementado

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# parcial2_mineria-de-datos
-parcial2_mineria de datos
+# Información
+Se extrajo data usando web scrapping de la página OLX
+
+Toda la información extraída está almacenada en una base de datos usando **supabase**
+
+La data puede ser manipulada desde la página de "tabla" en donde se encuentra un CRUD
+
+### Base de datos local
+
+Versión 1: **API Y BD local**
+[link a la API](https://github.com/zacksPerez43/info_renault-FLASK-API)
+
+### Problema con el CRUD
+El **update** no funciona ya que la URL de la API que ofrece **supabase** no la entiendo, por lo cual, no se pueden actualizar datos
+
+### Bug en el CRUD - UPDATE no funcional
+Cada vez que se actualiza un dato, se eliminaxd
--- javascript/valid-sesion.js
+++ javascript/valid-sesion.js
@@ -16,12 +16,15 @@ iniciarSesion.onclick = function() {
 		  }
 	  
 		  axios({
-			method: "POST",
-			url: "http://127.0.0.1:4000/iniciar_sesion",
-			data: datos_insertar
+			method: "GET",
+			url: "https://elofgzyhnliurwchnmse.supabase.co/rest/v1/usuarios?select=*",
+			headers: {
+				apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsb2ZnenlobmxpdXJ3Y2hubXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE1MDM5NTksImV4cCI6MTk5NzA3OTk1OX0.5zlT7FxiI7bD4as9wzXXKaprjKybRlUJe6Pnao_HhpI',
+				Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsb2ZnenlobmxpdXJ3Y2hubXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE1MDM5NTksImV4cCI6MTk5NzA3OTk1OX0.5zlT7FxiI7bD4as9wzXXKaprjKybRlUJe6Pnao_HhpI'
+			  }
 		  })
 			.then(res => {
-				if (res.data == "1"){
+				if (res.data[0].usuario == "Saray" && res.data[0].contrasena == "4368"){
 					location.href = "paginas/inicio.html";
 				}else{
 					console.log("el usuario y contraseña no existe papu")
